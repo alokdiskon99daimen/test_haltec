5948355b4f4ce970645fc00d2c8c539188c07dd5
Feiyyu <161292746+alokdiskon99daimen@users.noreply.github.com>
2025-11-18 17:25:42 +0700

    Enhance conn.php with comments for clarity
    Added comments for database connection and error handling.

--- test_haltec/conn.php
+++ test_haltec/conn.php
@@ -1,8 +1,9 @@
 <?php
-$conn = mysqli_connect("localhost","root","","test_haltec");
+$conn = mysqli_connect("localhost","root","","test_haltec"); //koneksi ke database
 
-if (mysqli_connect_errno()) {
+if (mysqli_connect_errno()) { //handling error koneksi database
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   exit();
 }
+
 ?>
